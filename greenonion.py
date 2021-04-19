72c19daf55605adea86b1b536be1324323e02758
Joey Shapiro <joeyashapiro@gmail.com>
2021-04-19 19:44:07 -0400

    maybe final changes but emails

--- greenonion.py
+++ greenonion.py
@@ -3,7 +3,7 @@ from email.message import EmailMessage
 from scapy.all import * # for sniffing the net
 import json # for the rules
 
-sender = 'sender@ourdomain.com'
+sender = 'project@localhost'
 
 banner = open('banner')
 f = open('rules.json', 'r') # json of the rules
@@ -26,10 +26,12 @@ def notifyEmail(to, ip):
     #who to send
     #send
     # email to be sent
-    message = 'Attention,', to, ', someone has gone to the bad IP:', ip, ', and it requires your futher attention. This information has been logged in the file.'
+    message = 'Attention, ' + to + ' , someone has gone to the bad IP: ' + ip + ' , and it requires your futher attention. This information has been logged in the file.'
     print(message)
     try:
         smtpObj = smtplib.SMTP('localhost')
+        header = 'To:' + to + '\n' + 'From:' + sender + '\n' + 'Subject:notice \n'
+        message = header + message
         smtpObj.sendmail(sender, to, message)
         print('sent notice to proper recipient')
     except SMTPException:
@@ -40,9 +42,13 @@ def print_pkt(pkt):
     # if ip is in list then notify proper
     try:
         destination = pkt['IP'].dst # IP destination
+        # make list just give website and look for each of that
         recipient = check_bad(destination)
         if(recipient != None):
-            notifyEmail(recipient, destination)
+            arr = recipient.split(',')
+            site = arr[0]
+            to = arr[1]
+            notifyEmail(to, site)
             wrpcap(flog, pkt, append=True) #flog.write(raw(pkt)) # figure out
     except: # if the pkt is just stupid or something
         pass
